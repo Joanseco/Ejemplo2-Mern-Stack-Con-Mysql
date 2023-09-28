05c4a97b8dcab59ee0aefc1a56cd33cdfd573174
Joanseco <joanseco5@gmail.com>
2023-09-28 18:26:55 -0500

    Agregue la funcionalidad de eliminar las tareas

--- client/src/pages/TasksPage.jsx
+++ client/src/pages/TasksPage.jsx
@@ -1,7 +1,36 @@
+import { useEffect, useState } from "react";
+import { getTasksRequest } from "../api/tasks.api";
+import TaskCard from "../components/TaskCard";
+
 function TasksPage() {
+  const [tasks, setTasks] = useState([]);
+
+  
+  useEffect(() => {
+    async function loadTasks() {
+      const response = await getTasksRequest();
+      setTasks(response.data);
+    }
+    loadTasks();
+  }, []);
+
+  function renderMain(){
+    
+    if(tasks.length===0){
+      return <h1>No hay tareas</h1>
+    }
+
+    return tasks.map((task) => (
+      <TaskCard task={task} key={task.id} />
+    ))
+  }
+
   return (
-    <div>TasksPage</div>
-  )
+    <div>
+      <h1>Tasks</h1>
+      {renderMain()}
+    </div>
+  );
 }
 
-export default TasksPage
+export default TasksPage;
